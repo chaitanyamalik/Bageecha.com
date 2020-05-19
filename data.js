7250747acfa75bf4cfbba63506db85bd67b6e11f
Chaitanya Malik <46478044+chaitanyamalik@users.noreply.github.com>
2020-05-20 02:11:34 +0530

    bageecha

---
+++ data.js
@@ -0,0 +1,32 @@
+module.exports = function () {
+    return {
+        products: [
+            { id: 1, name: "Frangipanis", category: "Flowers",description: "Known for their sweet fragrance and pure white color.", price: 275, imglink: "../../assets/images/Frangipanis.jpg" },
+            { id: 2, name: "Butea Monosperma", category: "Flowers",description: "This species is native to India and well known for healing disorders of several kinds", price: 48.95, imglink: "../../assets/images/butea monosperma.jpg" },
+            { id: 3, name: "Plumbago", category: "Plants",description: "Plumbago is a shrub which is also known as Sky flower in a layman’s language.", price: 19.50, imglink: "../../assets/images/Plumbago.jpg" },
+            { id: 4, name: "Pennisetum Setaceum", category: "Plants",description: "This plant is also known as fountain grass.",price: 34.95, imglink: "../../assets/images/Pennisetum Setaceum.jpg" },
+            { id: 5, name: "Ascocenda Princess Misaka", category: "Flowers",description: "Ascocenda Princess Misaka is a beautiful flower native to India, known for its large purple or pink flowers.", price: 79500, imglink: "../../assets/images/Ascocenda Princess Misaka.jpg" },
+            { id: 6, name: "Ganges Primrose", category: "Flowers",description: "The leaves are short and green and the petals are pure white.", price: 16, imglink: "../../assets/images/Ganges Primrose.jpg" },
+            { id: 7, name: "Dianella tasmanica (Tasman Flax-lily)", category: "Plants",description: "One can easily grow this plant with least of the efforts; enough water and lots of sunlight.",price: 29.95, imglink: "../../assets/images/Dianella tasmanica (Tasman Flax-lily).jpg" },
+            { id: 8, name: "Ophiopogon (Lillyturf)", category: "Plants",description: "This plant is extremely beautiful, especially due to their vibrant green colour.", price: 75, imglink: "../../assets/images/Ophiopogon (Lillyturf).jpg" },
+            { id: 9, name: "Lotus", category: "Flowers",description: "Lotus is the national flower of India and has often been called Sacred Lotus.", price: 1200, imglink: "../../assets/images/Lotus.jpg" },
+            { id: 10, name: "Jasmine", category: "Flowers",description: "The fresh and gorgeous Jasmine flowers are the pride of India", price: 1200, imglink: "../../assets/images/Jasmine.jpg" },
+            { id: 11, name: "Hibiscus", category: "Flowers",description: "It is known for its unbelievable colors and gorgeous appearance.", price: 1200, imglink: "../../assets/images/Hibiscus.jpg" },
+            { id: 12, name: "Sunflowers", category: "Flowers",description: "Sunflower is the most beautiful flower in India and is known for its fiery bloom.", price: 1200, imglink: "../../assets/images/Sunflowers.jpg" },
+            { id: 13, name: "Fox Brush Orchid", category: "Flowers",description: "Crystalline pink in color and fall among the dwarf species category.", price: 1200, imglink: "../../assets/images/Fox Brush Orchid.jpg" },
+            { id: 14, name: "Jungle Germanium", category: "Flowers",description: "Jungle Germanium has often been regarded as flame of the woods or jungle flame.", price: 1200, imglink: "../../assets/images/Jungle Germanium.jpg" },
+            { id: 15, name: "Pandanus", category: "Plants",description: "The plant bears flowers having a sweet, perfumed smell.", price: 1200, imglink: "../../assets/images/Pandanus.jpg" },
+            { id: 16, name: "Pittosporum Tobira", category: "Plants",description: "Pittosporum is known by a few names likes Australian shrub, Japanese pittosporum, deride orange and Japanese cheesewood.", price: 1200, imglink: "../../assets/images/Pittosporum Tobira.jpg" },
+            { id: 17, name: "Asparagus Densiflorous", category: "Plants",description: "Asparagus densiflorus is otherwise called by many names like asparagus plant, tuft asparagus, foxtail greenery and so on.", price: 1200, imglink: "../../assets/images/Asparagus Densiflorous.jpg" },
+            { id: 18, name: "Ipomoea", category: "Plants",description: "This plant is also known as morning flower.", price: 1200, imglink: "../../assets/images/Ipomoea.jpg" },
+            { id: 19, name: "Alterananthera", category: "Plants",description: "Grown prominently for their attractive, colourful leaves, which are usually toothed, and varies in size.", price: 1200, imglink: "../../assets/images/Alterananthera.jpg" },
+            { id: 20, name: "Iresene", category: "Plants",description: "It bears extremely beautiful foliage in different shades of red and green.", price: 1200, imglink: "../../assets/images/Iresene.jpg" },
+            { id: 21, name: "Beetroot", category: "Vegetable Seeds",description: "Beetroot prefer to be grown in moist, fertile soil in a sunny spot, but will also thrive in raised beds or pots.", price: 1200, imglink: "../../assets/images/Beetroot.png" },
+            { id: 22, name: "Bitter Gourd", category: "Vegetable Seeds",description: "Prefer to be grown in moist, fertile soil in a sunny spot, but will also thrive in raised beds or pots.", price: 1200, imglink: "../../assets/images/Bitter Gourd.jpg" },
+            { id: 23, name: "Bottle Guard", category: "Vegetable Seeds",description: "Prefer to be grown in moist, fertile soil in a sunny spot, but will also thrive in raised beds or pots.", price: 1200, imglink: "../../assets/images/Bottle Guard.jpg" },
+            { id: 24, name: "Radish", category: "Vegetable Seeds",description: "Prefer to be grown in moist, fertile soil in a sunny spot, but will also thrive in raised beds or pots.", price: 1200, imglink: "../../assets/images/Radish.jpg" },
+            { id: 25, name: "Tomato", category: "Vegetable Seeds",description: "Prefer to be grown in moist, fertile soil in a sunny spot, but will also thrive in raised beds or pots.", price: 1200, imglink: "../../assets/images/Tomato.jpg" },
+        ],
+        orders: []
+    }
+}
